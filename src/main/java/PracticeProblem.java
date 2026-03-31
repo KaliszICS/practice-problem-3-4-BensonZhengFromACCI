68d5ba6b9e3c43a0a47623995aca415b3803bdcf
BensonZhengFromACCI <343804126@tdsb.ca>
2026-03-31 01:08:56 +0000

    hi mr kalisz i watched a youtube tutorial

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -4,4 +4,35 @@ public class PracticeProblem {
 
 	}
 
+	public static int min(int num1, int num2, int num3) {
+		if (num1 <= num2 && num1 <= num3) {
+			return num1;
+		}
+			else
+				if (num2 <= num1 && num2 <= num3) {
+					return num2;
+				}
+	
+		else {
+		return num3;
+		}
+	}
+	
+
+	public static boolean isLeapYear(int num1) {
+		if (num1 % 4 == 0) {
+			if (num1 % 100 == 0) {
+				if (num1 % 400 == 0) {
+					return true;
+				}
+				else {
+					return false;
+				}
+			}
+			else return true;
+		}
+		else return false;
+	}
 }
+
+
